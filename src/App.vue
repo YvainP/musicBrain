adf7921771efbee4ca15bfe6f7ebc702029eb5d7
nano <i20-10@hotmail.fr>
2020-02-28 16:44:12 +0100

    page d'accueil css :(

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,3 @@
   </div>
   
 </template>
-
-<script>
-
-
-export default {
-  
-  name: 'app',
-  data () {
-    return {
-    }
-  },
-
-}
-</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,27 @@
 <template>
 
-  <div id="app">    
+  <div id="app" class="text-center">    
     <router-view></router-view>
-    <h2 class="title">M</h2>
-    <router-link to="/">home</router-link>
-    <router-link to="/research">recherche</router-link>
+    <h1 class="bigTitle">MusicBrain app</h1>
+    <p class="subTitle">Welcome on my music application, 
+    you can <br> search for a music/artist/album ...etc</p>
+    <p class="subTitle">Give it a try with the field below :)
+    </p>
+    <div class="group">      
+      <input type="text" required>
+      <span class="bar"></span>
+      <label>Any keywords in here</label>
+    </div>
+    <router-link to="/research" class="btn btn-dark">
+      Let's search it!
+    </router-link>
   </div>
   
 </template>
 
 <script>
 
+import styles from "../css/accueil.css";
 
 export default {
   
