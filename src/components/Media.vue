477850b536f4ec0571a4bce33db1af09152836f9
YvainP <i20-10@hotmail.fr>
2020-03-03 22:04:24 +0100

    ajout des icones, faire un helper icone

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -1,21 +1,27 @@
 <template>
-  <!-- En fonction du média on récupère les champs intéressants-->
+  <!-- En fonction du média on récupère les champs intéressants
+  On gère le cas des artistes -->
   <div v-if="typeMedia == 'artist'">
-    <div v-if="mediaData.name == 'Group'">
-      <i class="fas fa-music"></i>
-    </div>
+    <!-- Affiche un icône cohérent au type de l'artiste -->
+    <i v-if="mediaData.type == 'Group'" class="fas fa-users"></i>
+    <i v-if="mediaData.type == 'Choir'" class="fas fa-music"></i>
+    <i v-if="mediaData.type == 'Other'" class="fas fa-question"></i>
+    <i v-if="mediaData.type == 'Person'" class="fas fa-user"></i>
+    <i v-if="mediaData.type == 'Character'" class="fas fa-theater-masks"></i>
+    <i v-if="mediaData.type == 'Orchestra'" class="fas fa-music"></i>
     Name: {{ mediaData.name }} 
     Country: {{ mediaData.area.name }}
     Musical genre: {{ mediaData.disambiguation }}
   </div>
+  <!-- Gère le cas d'une musique -->
   <div v-else-if="typeMedia == 'recording'">
+    <i class="fas fa-music"></i>
     Song name: {{ mediaData.title }}
     Name of the artist: {{ mediaData["artist-credit"][0].name }}
     Musical genre: 
   </div>
 </template>
 <script>
-
 
 export default {
     name: 'app',
@@ -25,6 +31,5 @@ export default {
         }
     },
 }
-
 </script>
 
