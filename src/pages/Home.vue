895da2621c3b031bbe1101fde2a4f61635c7ce33
nano <i20-10@hotmail.fr>
2020-03-12 16:50:53 +0100

    more css

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,22 @@
 <!-- Ceci est la page d'accueil -->
 <template>
   <div id="app" class="text-center">    
-    <h1 class="bigTitle">MusicBrain app</h1>
+    <h1 class="bigTitle">Shiku app</h1>
     <p class="subTitle">
       Welcome on my music application, 
       you can <br> search for a music/artist/album ...etc
     </p>
-    <p class="subTitle">Give it a try with the field below :)</p>
-
+    <p class="subTitle">
+      <br>Give it a try with the field below :)
+    </p>
     <form class="group">
       <input ref="search" id="inputSearch" type="text" placeholder="Enter any keywords" 
-        v-model="inputWords" autofocus/>
+        v-model="inputWords"/>
       <span class="bar"></span>
       <!-- On lie la page de recherche au "bouton" en lui passant 
       comme paramètres ce que l'user a tapé dans la barre -->
       <router-link :to="{ name: 'research', params: { firstSearch: inputWords } }">
-        <button class="btn btn-dark mt-4">
+        <button class="btn btn-dark mt-5 cssBtn">
           Let's search it!
         </button>
       </router-link>
